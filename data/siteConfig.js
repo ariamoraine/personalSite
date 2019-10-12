4e82f9f815f25537ff9434feac06ad54590ee39e
Aria Moraine <ariamoraine@gmail.com>
2019-10-11 21:50:55 -0400

    first setup for gatsby

---
+++ data/siteConfig.js
@@ -0,0 +1,98 @@
+module.exports = {
+  siteTitle: 'Hi! I\'m Aria!',
+  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'Aria Corti',
+  githubUsername: 'ariamoraine',
+  authorAvatar: '/images/headshot.jpg',
+  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
+    In 2012 I started working as a support technician and I approached the developers.
+    In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
+    I'm currently working as a product developer with <strong>Javascript, React, and mobX.</strong>`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 70
+    },
+    {
+      name: 'CSS',
+      level: 70
+    },
+    {
+      name: 'Javascript',
+      level: 90
+    },
+    {
+      name: 'NodeJs',
+      level: 80
+    },
+    {
+      name: 'React',
+      level: 90
+    },
+    {
+      name: 'Git',
+      level: 70
+    },
+    {
+      name: 'Java',
+      level: 50
+    },
+    {
+      name: 'python',
+      level: 50
+    }
+  ],
+  jobs: [
+    {
+      company: "Protenus",
+      begin: {
+        month: 'feb',
+        year: '2018'
+      },
+      duration: null,
+      occupation: "Product developer",
+      description: "Maintain an admin and product UI that is used by over 50 large scale hospital systems. Whose privacy oﬀicers use the Protenus platform on a daily basis to complete critical work <br> Added new sections and features to the core product every quarter. One example being ‘Groups’ - which added a layer of abstraction for organizations with multiple locations. Enabling, user controlled full application filtering of unwanted data. <br> Assisted in bringing to market the second product for the company. Added into the existing architecture while maintaining the quality and reliability of the core privacy product <br> Managed by-weekly product releases. Creating and maintaining internal documentation of tickets completed within the sprint as well as leading release meetings with the customer support team. Relaying all necessary information for the external release notes <br> Maintained quality product enabling the company to reach Best In Klas Category Leader of 2019 <br> Scrum Master, run all Agile meetings, prioritized/vetted new requests, balanced the team assignments between the three products, tech debt, and feature requests <br> Completed UX/UI design tickets working with sketch and invision"
+
+    }, {
+      company: "Fullstack Academy of Code",
+      begin: {
+        month: 'Jun',
+        year: '2018'
+      },
+      duration: '5 mos',
+      occupation: "Foundations Mentor (Part time, Nights and Weekends)",
+      description: " Provided   a   first   point   of   contact   for   a   cohort   of   15   -   20   students   in   month   long   sessions   while   working   through   the   at-home   javaScript   curriculum    + Graded   all   tests,   ensuring   students   understood   all   material   and   followed   JavaScript   best   practices.   Leaving   constructive   feedback   on   areas   to   improve   their   skills   and   understanding   + One   on   One   tutoring   for   students   that   struggled   with   any   topics"
+    }, {
+      company: "Movatic",
+      begin: {
+        month: 'Aug',
+        year: '2017'
+      },
+      duration: '7 mos',
+      occupation: "Full-Stack Engineer",
+      description: " Rewrote   the   existing   angular   administrative   website   with   react   and   react-redux,   bringing   it   to   full   feature   parity   as   well   as   adding   additional   features   + Worked   independently,   completing   tasks   and   assignments   with   quick   turnaround   and   minimal   assistance   + Envisioned   and   executed   better   UI/UX   + Used   existing   python   backend,   writing   and   maintaining   REST   API "
+  
+    },
+    /* ... */
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/aria-moraine",
+    github: "https://github.com/ariamoraine",
+    email: "ariamoraine@gmail.com"
+  },
+  // siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  // pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Aria Corti',
+      url: '/',
+    }
+  ]
+}
